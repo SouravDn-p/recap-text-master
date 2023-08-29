bca5834b667e7b60edf7485debf6eab13c135bf2
SD  Creation's <107785936+SouravDn-p@users.noreply.github.com>
2023-08-29 21:50:39 +0600

    Updates

---
+++ precache-manifest.640633861fced24d8d1bac84707c2107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a139e8e629a5dc54c83b",
+    "url": "/SouravDn-p/recap-text-master/static/css/main.9f9b7cfe.chunk.css"
+  },
+  {
+    "revision": "a139e8e629a5dc54c83b",
+    "url": "/SouravDn-p/recap-text-master/static/js/main.a139e8e6.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/SouravDn-p/recap-text-master/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/SouravDn-p/recap-text-master/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "439a518865a52fd36d29",
+    "url": "/SouravDn-p/recap-text-master/static/js/runtime~main.439a5188.js"
+  },
+  {
+    "revision": "4bf6fe392f769fa25303c924b3268e99",
+    "url": "/SouravDn-p/recap-text-master/index.html"
+  }
+];
