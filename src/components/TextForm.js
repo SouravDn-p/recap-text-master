21e556f8989eec95b00b634471157eb391a318b3
SD  Creation's <107785936+SouravDn-p@users.noreply.github.com>
2023-09-02 22:01:55 +0600

    final recap

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 export default function TextForm(props) {
 
     const handleCopy= ()=>{
-      let text = document.getElementById('myBox');
-      navigator.clipboard.writeText(text.value);
+      // let text = document.getElementById('myBox');
+      navigator.clipboard.writeText(text);
       props.showAlert("The text is copied successfully!","success")
     }
     const removeExtraSpaces= ()=>{
@@ -24,23 +24,24 @@ return (
       <>
         <div>
             <div className="mb-3">
-                <h1 className=' mt-3 mb-3'>{props.heading}</h1>
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <h1 className=' mt-3 mt-3'>{props.heading}</h1>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor:props.mode === "dark" ?'#02101b':'white',
+             color:props.mode === "light" ?'black':'white'}}  >  </textarea>
             </div>
-            <button className='btn btn-primary' onClick={()=>setText(text.toUpperCase())}> Convert to UpperCase </button>
-            <button className='btn btn-success mx-3' onClick={()=>{
+            <button className='btn btn-primary mx-3 mt-3' onClick={()=>setText(text.toUpperCase())}> Convert to UpperCase </button>
+            <button className='btn btn-success mx-3 mt-3' onClick={()=>{
               setText(text.toLowerCase());
               }}> Convert to LowerCase </button>
 
-            <button className='btn btn-warning ' onClick={handleCopy}>copy text</button>
-            <button className='btn btn-secondary mx-3' onClick={removeExtraSpaces}>Remove Extra Spaces</button>
-            <button className='btn btn-danger ' onClick={()=>setText('')}> Clear Text </button>
+            <button className='btn btn-warning mt-3' onClick={handleCopy}>copy text</button>
+            <button className='btn btn-secondary mx-3 mt-3' onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+            <button className='btn btn-danger mt-3' onClick={()=>setText('')}> Clear Text </button>
         </div>
 
-        <div className="container mt-3">
+        <div className="container mt-3 ">
             <h2>Your text summary</h2>
-            <h5>{text.split(" ").length} Word and {text.length} characters</h5>
-            <h6>{0.008 * text.split(" ").length} minutes read</h6>
+            <h5>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Word and {text.length} characters</h5>
+            <h6>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</h6>
             <h2>Preview</h2>
             <p>{text}</p>
         </div>
